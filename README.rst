ada376ac9a2d0ed092e18449f5a31696506522e5
Alex Raichev <alex@raichev.net>
2015-12-25 16:03:25 +1300

    Updated tasks.py

--- README.rst
+++ README.rst
@@ -5,6 +5,8 @@ A website dedicated to Markov poems (definition included on website).
 
 Requirements
 ------------
+The Python packages listed in ``requirements.txt`` plus
+
 - `RapydML <https://bitbucket.org/pyjeon/rapydml>`_ for compiling RapydML (``.pyml``) files to HTML
 - `RapydCSS <https://bitbucket.org/pyjeon/rapydcss>`_ for compiling RapydCSS (``.sass``) files to CSS
 - `RapydScript <https://github.com/atsepkov/RapydScript>`_ for compiling RapydScript (``.pyj``) files to JavaScript
--- tasks.py
+++ tasks.py
@@ -1,4 +1,30 @@
+import yaml
+import json
+import os
+
 from invoke import run, task
+
+
+YAML_DIR = 'poems/yaml/'
+JSON_DIR = 'poems/jsonized/'
+
+@task
+def jsonize_poems():
+    """
+    Convert YAML-formatted poems to JSON-formatted poems.
+    """
+    for filename in os.listdir(YAML_DIR):
+        in_path = os.path.join(YAML_DIR, filename)
+        if not os.path.isfile(in_path):
+            continue
+        if not in_path.endswith('.yaml'):
+            continue
+        with open(in_path, 'r') as f:
+            print('Converting {!s} to JSON'.format(filename))
+            y = yaml.load(f)
+            out_path = os.path.join(JSON_DIR, 
+              filename.replace('.yaml', '.json'))
+            json.dump(y, open(out_path, 'w'))
 
 @task
 def push_github():
